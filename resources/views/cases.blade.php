01c06ab7ad80950e7fc41d5eaa2a7d77e0ebe356
wxf <809602518@qq.com>
2017-11-27 14:46:50 +0800

    update

--- resources/views/cases.blade.php
+++ resources/views/cases.blade.php
@@ -39,6 +39,15 @@
 </div>
 
 <script type="text/javascript">
+    var $sltList = $('.slt-list'),$sltItem = $sltList.find('.slt-item'),
+        sltHeight = 60;
+
+    if($sltList.find(".slt-item.active .slt-list").length){
+        sltHeight = $sltList.find(".slt-item.active .slt-list").outerHeight();
+    }else{
+        sltHeight = $sltList.outerHeight();
+    }
+    $sltList.closest(".page-aside").css("min-height",sltHeight);
 
 </script>
 @stop

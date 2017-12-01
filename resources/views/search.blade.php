3d0e35141cc3f98c3caae026f0208610a1d93ca2
wxf <809602518@qq.com>
2017-12-01 20:08:21 +0800

    update

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.default')
-@section('title', is_null($key) ? "搜索" : $key)
+@section('title', $key)
 @section('content')
 <div class="page-main page-search">
     <div class="search-hd">
@@ -10,19 +10,7 @@
         @endif
     </div>
     <div class="search-bd">
-        @foreach($contents as $con)
-            <div class="search-item">
-                @if( $con->column->parent_id == 3 || ($con->column->parent_id == 0 && $con->column->id == 2 ))
-                <a class="search-link" attr="{{ $con->column->id }}" href="/{{ $con->column->path ? $con->column->path : $con->column->id }}#{{ $con->id }}">
-                    {{$con->title}}
-                </a>
-                @else
-                <a class="search-link" attr="{{ $con->column->id }}" href="/{{ $con->column->path ? $con->column->path : $con->column->id }}/{{ $con->path ? $con->path : $con->id }}">
-                    {{$con->title}}
-                </a>
-                @endif
-            </div>
-		@endforeach
+        
     </div>
 </div>
 @stop

350641fe8da69678b5c80e816af3cdea65cc3466
wxf <809602518@qq.com>
2017-12-01 20:12:18 +0800

    update

--- app/Http/Controllers/Web/SearchController.php
+++ app/Http/Controllers/Web/SearchController.php
@@ -24,7 +24,7 @@ class SearchController extends Controller
             $contents = $cons->where('title','like','%'.$key.'%')->get();
 
         }else{
-            $contents = $cons->where('title','!=','')->get();
+            $contents = $cons->where('title','<>','')->get();
             $key = '';
         }
         dd($contents);

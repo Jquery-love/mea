8eef20f3530b7af34ce60af96014dbfaa9dcc7e5
wxf <809602518@qq.com>
2017-12-01 19:52:55 +0800

    update

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -6,19 +6,21 @@
         您当前正在搜索：<span>{{ $key }}</span>
     </div>
     <div class="search-bd">
-        @foreach($contents as $con)
-            <div class="search-item">
-                @if( $con->column->parent_id == 3 || ($con->column->parent_id == 0 && $con->column->id == 2 ))
-                <a class="search-link" attr="{{ $con->column->id }}" href="/{{ $con->column->path ? $con->column->path : $con->column->id }}#{{ $con->id }}">
-                    {{$con->title}}
-                </a>
-                @else
-                <a class="search-link" attr="{{ $con->column->id }}" href="/{{ $con->column->path ? $con->column->path : $con->column->id }}/{{ $con->path ? $con->path : $con->id }}">
-                    {{$con->title}}
-                </a>
-                @endif
-            </div>
-		@endforeach
+        @if(!empty($contents))
+            @foreach($contents as $con)
+                <div class="search-item">
+                    @if( $con->column->parent_id == 3 || ($con->column->parent_id == 0 && $con->column->id == 2 ))
+                    <a class="search-link" attr="{{ $con->column->id }}" href="/{{ $con->column->path ? $con->column->path : $con->column->id }}#{{ $con->id }}">
+                        {{$con->title}}
+                    </a>
+                    @else
+                    <a class="search-link" attr="{{ $con->column->id }}" href="/{{ $con->column->path ? $con->column->path : $con->column->id }}/{{ $con->path ? $con->path : $con->id }}">
+                        {{$con->title}}
+                    </a>
+                    @endif
+                </div>
+    		@endforeach
+        @endif
     </div>
 </div>
 @stop

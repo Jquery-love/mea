7212b320c498c52a01978688177e6f6e2a9ee809
wxf <809602518@qq.com>
2017-12-01 20:01:03 +0800

    update

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.default')
-@section('title', $key)
+@section('title', is_null($key) ? "搜索" : $key)
 @section('content')
 <div class="page-main page-search">
     <div class="search-hd">
-        您当前正在搜索：<span>{{ $key }}</span>
+        @if(is_null($key))
+            请输入要搜索内容
+        @else
+            您当前正在搜索：<span>{{ $key }}</span>
+        @endif
     </div>
     <div class="search-bd">
-        @if(!empty($contents) && !is_null($contents))
-            @foreach($contents as $con)
-                <div class="search-item">
-                    @if( $con->column->parent_id == 3 || ($con->column->parent_id == 0 && $con->column->id == 2 ))
-                    <a class="search-link" attr="{{ $con->column->id }}" href="/{{ $con->column->path ? $con->column->path : $con->column->id }}#{{ $con->id }}">
-                        {{$con->title}}
-                    </a>
-                    @else
-                    <a class="search-link" attr="{{ $con->column->id }}" href="/{{ $con->column->path ? $con->column->path : $con->column->id }}/{{ $con->path ? $con->path : $con->id }}">
-                        {{$con->title}}
-                    </a>
-                    @endif
-                </div>
-    		@endforeach
-        @endif
+        @foreach($contents as $con)
+            <div class="search-item">
+                @if( $con->column->parent_id == 3 || ($con->column->parent_id == 0 && $con->column->id == 2 ))
+                <a class="search-link" attr="{{ $con->column->id }}" href="/{{ $con->column->path ? $con->column->path : $con->column->id }}#{{ $con->id }}">
+                    {{$con->title}}
+                </a>
+                @else
+                <a class="search-link" attr="{{ $con->column->id }}" href="/{{ $con->column->path ? $con->column->path : $con->column->id }}/{{ $con->path ? $con->path : $con->id }}">
+                    {{$con->title}}
+                </a>
+                @endif
+            </div>
+		@endforeach
     </div>
 </div>
 @stop

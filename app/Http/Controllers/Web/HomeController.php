b43083a4556f2263709b83f193f24ef598bcb463
wxf <809602518@qq.com>
2017-12-01 19:42:18 +0800

    update

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
     //
     public function index(Column $column,Content $content,Files $files){
         $colhd = $column->where('parent_id',0)->orderBy('sort','asc')->find([1, 2, 3,4,5,6,10]);
-        $colft = $column->where('parent_id',0)->orderBy('sort','asc')->find([6,33,5,9]);
+        $colft = $column->where('parent_id',0)->orderBy('sort','asc')->find([6,33,2,9]);
         $banners = $files->where([['application','=',2],['column_id','=',0]])->orderBy('sort','asc')->get();
         $company = $column->find(10);
         $cases = $content->whereIn('column_id',[9])->where('recommend',1)->orderBy('updated_at','desc')->get();

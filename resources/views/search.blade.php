1c5c3e21ee61f90bbf829a70a192e32a2838c4cb
wxf <809602518@qq.com>
2017-12-01 20:09:53 +0800

    update

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -10,7 +10,19 @@
         @endif
     </div>
     <div class="search-bd">
-        
+        @foreach($contents as $con)
+            <div class="search-item">
+                @if( $con->column->parent_id == 3 || ($con->column->parent_id == 0 && $con->column->id == 2 ))
+                <a class="search-link" attr="{{ $con->column->id }}" href="/{{ $con->column->path ? $con->column->path : $con->column->id }}#{{ $con->id }}">
+                    {{$con->title}}
+                </a>
+                @else
+                <a class="search-link" attr="{{ $con->column->id }}" href="/{{ $con->column->path ? $con->column->path : $con->column->id }}/{{ $con->path ? $con->path : $con->id }}">
+                    {{$con->title}}
+                </a>
+                @endif
+            </div>
+		@endforeach
     </div>
 </div>
 @stop
